8c864690f8105a42457a6e4f60780992a0d94de9
Julian Williams <julian.th.williams@gmail.com>
2022-01-02 11:51:06 -0800

    Added Util mutations and queries

---
+++ client/src/utils/mutations.js
@@ -0,0 +1,50 @@
+import { gql } from '@apollo/client';
+
+export const LOGIN_USER = gql`
+  mutation login($email: String!, $password: String!) {
+    login($email: String!, $password: String!) {
+      token
+      user{
+          _id
+          username
+      }
+      
+    }
+  }
+`;
+
+export const SAVE_BOOK = gql`
+  mutation saveBook($input: SavedBookInput!) {
+    createVote($input: SavedBookInput!) {
+     _id
+     username
+     email
+     savedbooks {
+        bookId
+        title
+        authors
+        image
+        description
+        link  
+     }
+    }
+  }
+`;
+
+export const REMOVE_BOOK = gql`
+  mutation removeBook($bookId: String!) {
+    createVote($bookId: String!) {
+     _id
+     username
+     email
+     savedbooks {
+        bookId
+        authors
+        image
+        description
+        title
+        link  
+     }
+    }
+  }
+`;
